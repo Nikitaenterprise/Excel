9c4aceec884b07d6f36736569ec6f6e1e8261c8e
Nikitaenterprise <prof.frimen@mail.ru>
2019-07-18 13:47:47 +0300

    Working on class hierarchy
    ExcelBook
    TKE

--- Excel.py
+++ Excel.py
@@ -1,11 +1,17 @@
 import openpyxl
-import re
 
-class ExcelScripts:
+
+class ExcelBook:
 
     def __init__(self, name: str):
         self.wb = openpyxl.load_workbook(name)
         self.ws = wb[wb.sheetnames[0]]
+
+    def getMaxRowForSheet(self, ws):
+        return ws.max_row
+
+    def getMaxColForSheet(self, ws):
+        return ws.max_col
 
     def save(self, name: str):
         self.wb.save(name)
@@ -13,7 +19,8 @@ class ExcelScripts:
         return
 
     def setCellsInColumnByRowCoord(self, rowCoord: int, sheetRange: str, value):
-        # Finds 
+        # Finds values in one column (sheetRange) by row coordinate (rowCoord)
+        # and then set value of that cell to (value)
         for cells in self.ws[sheetRange]:
             for cell in cells:
                 if cell.row == rowCoord:
@@ -21,11 +28,39 @@ class ExcelScripts:
         return
 
     def setCellsInRowByColumnCoord(self, columnCoord: int, sheetRange: str, value):
+        # Finds values in one row (sheetRange) by column coordinate (columnCoord)
+        # and then set value of that cell to (value)
         for cells in self.ws[sheetRange]:
             for cell in cells:
                 if cell.column == rowCoord:
                     cell.value = value
         return
+
+    def findCellByStr(self, str: str):
+        # Finds first cell by searchin in whole sheet the target (str)
+        for cells in self.ws:
+            for cell in cells:
+                if cell.value == str:
+                    return cell
+
+
+class TKE:
+
+    def __init__(self, wbName: str):
+        self.excelBook = ExcelBook(wbName)
+        self.header = excelBook.ws['A1':'BU9']
+
+
+    def restructurization(self):
+        for cells in excelBook.ws['H12:H22']:
+            for cell in cells:
+                if cell.value != None:
+                    excelBook.updateCell(cell.row, excelBook.ws)
+        return
+
+    def start(self):
+        restructurization()
+        
 
 
 def updateCell(coord, activeSheet):
@@ -39,7 +74,6 @@ def restructurization(activeSheet):
     for cellObj in activeSheet['F12':'F22']:
         for cell in cellObj:
             if cell.value != None:
-                print(cell, cell.coordinate, cell.value)
                 updateCell(cell.row, activeSheet)
     return
 
@@ -56,10 +90,18 @@ totalRowNumber = activeSheet.max_row
 wb2 = openpyxl.load_workbook('./second.xlsx')
 activeSheet2 = wb2[wb2.sheetnames[0]]
 
-activeSheet.insert_cols(openpyxl.utils.column_index_from_string('I'))
-for cell in activeSheet2['I']:
-   activeSheet.cell(row = cell.row, column = cell.column, value = cell.value, )
+activeSheet.insert_cols(openpyxl.utils.column_index_from_string('H'))
+#for cell in activeSheet2['I']:
+#   activeSheet.cell(row = cell.row, column = cell.column-1, value = cell.value)
 
-
+for cell in activeSheet['J']:
+    oldColumn = openpyxl.utils.get_column_letter(cell.column)
+    newColumn = openpyxl.utils.get_column_letter(cell.column + 1)
+    newAdress = str(newColumn) + str(cell.row)
+    
+    print(oldColumn, str(cell.row), cell.value, newColumn, newAdress, activeSheet[str(oldColumn)+str(cell.row)].value)
+    
+    activeSheet[newAdress] = openpyxl.formula.translate.Translator(cell.value, origin=str(oldColumn)+str(cell.row)).translate_formula(newAdress)
+    print(oldColumn, str(cell.row), cell.value, newColumn, newAdress)
 wb.save('out.xlsx')
 wb.close()
