44b2a49494c0af242160d2d621480bab38f5f17f
Nikitaenterprise <prof.frimen@mail.ru>
2019-07-19 14:21:34 +0300

    Formula modification

--- Excel.py
+++ Excel.py
@@ -101,6 +101,7 @@ def checkRangeInFormulas(ws, range: str, columnsWithFormulas):
     """Check a list with coordinates wich contains forulas with 
     some kind of ranges for intersection with modified 
     range after insertion of new column
+    This function shoul be used after insertion of the new column
 
     Keyword arguments:
     ws -- active worksheet
@@ -110,15 +111,51 @@ def checkRangeInFormulas(ws, range: str, columnsWithFormulas):
     cells with formulas wich contains ranges
 
     """
+    columnsList = []
     split = range.split(':')
     minCoordinate = openpyxl.utils.coordinate_to_tuple(split[0])
     maxCoordinate = openpyxl.utils.coordinate_to_tuple(split[1])
     minColumn, maxColumn = minCoordinate[1], maxCoordinate[1]
     for coordinate in columnsWithFormulas:
-        column = openpyxl.utils.coordinate_to_tuple(coordinate)
-        if column[1] < minColumn:
-            print(column, minColumn)
-            print('fff')
+        column = openpyxl.utils.coordinate_to_tuple(coordinate)[1]
+        if column < minColumn:
+            columnsList.append(openpyxl.utils.get_column_letter(column))
+    return columnsList
+
+def modifyFormulaInColumnFterInsertion(ws, targetColumns, newColumnCoordinate: str):
+    """Function looks throug whole column and if there is a cell with formula
+    like =A1+B1 and if column has been inserted between A1 and B1 then it will
+    modify formula to =A1+C1
+
+    Keyword arguments:
+    ws -- active worksheet
+    targetColumns -- sequence of columns that should be modified
+    newColumnCoordinates -- integer that corresponds to number of inserted columns
+
+    """
+    for column in targetColumns:
+        for cell in ws[column]:
+            print('beginning of cycle', cell.value)
+            token = openpyxl.formula.Tokenizer(str(cell.value))
+            if cell.value == None:
+                continue
+            newCellValue = str('=')
+            for element in token.items:
+                print('beginning if subsycle', element.value, element.type, element.subtype)
+                if element.subtype == 'RANGE':
+                    coord = openpyxl.utils.coordinate_to_tuple(element.value)
+                    print('coord = ', coord)
+                    newColumn = coord[1] + newColumnCoordinate
+                    print('newColumn = ', newColumn)
+                    newCoordinate = str(openpyxl.utils.get_column_letter(newColumn) + str(coord[0]))
+                    newCellValue+=newCoordinate
+                else:
+                    newCellValue+=element.value
+                print('newCellValue = ', newCellValue)
+                print('exit subloop')
+            cell.value = newCellValue
+            print('exit loop')
+            print(cell.value)
     return
 
 # Opening workbook at sheet 1
@@ -141,8 +178,8 @@ activeSheet2 = wb2[wb2.sheetnames[0]]
 #   activeSheet.cell(row = cell.row, column = cell.column-1, value = cell.value)
 columns = findColumnsWithFormulas(activeSheet)
 activeSheet.move_range('J12:L22', rows=0, cols=1, translate=True)
-checkRangeInFormulas(activeSheet, 'J12:L22', columns)
-
+columnsList = checkRangeInFormulas(activeSheet, 'J12:L22', columns)
+modifyFormulaInColumnFterInsertion(activeSheet, columnsList, 1)
 #for cell in activeSheet['J']:
 #    oldColumn = openpyxl.utils.get_column_letter(cell.column)
 #    newColumn = openpyxl.utils.get_column_letter(cell.column + 1)
