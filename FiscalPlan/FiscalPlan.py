3636ccc76aa48d8787c97b1b6fe2d3a118655622
Лужанский Никита Вячеславович <LuzhanskyiM@naftogaz.com>
2019-09-10 12:07:24 +0300

    Working on financial plan algorithm

--- FiscalPlan/FiscalPlan.py
+++ FiscalPlan/FiscalPlan.py
@@ -6,16 +6,46 @@ import win32com.client
 
 from Modules.ExcelBook import ExcelBook
 #from Excel.Modules.Header import Header
-sys.path.clear()
 
 class FiscalPlan:
     
-    def __init__(self, wbName):
-        self.wb = ExcelBook(wbName, data_only=True)
+    def __init__(self, fiscalPlanExcel: str, todayCashExcel: str, 
+                PATExcel: str, SBUTExcel: str, TEZExcel: str):
+        self.fiscalPlan = ExcelBook(fiscalPlanExcel, read=False)
+        self.todayCash = ExcelBook(todayCashExcel, read=False)
+        self.PAT = ExcelBook(PATExcel, read=False)
+        self.SBUT = ExcelBook(SBUTExcel, read=False)
+        self.TEZ = ExcelBook(TEZExcel, read=False)
     
     def run(self):
+        self.todayCash.readExcelFile()
+        print(self.PopulationAndReligion())
+
 
         return
+
+    def PopulationAndReligion(self):
+        """Fins sum of cash from population and religion
+        """
+        populationRow = self.todayCash.findCellByStr("1.2. Населення", "C").row
+        populationColumn = openpyxl.utils.column_index_from_string(str("J"))
+        populationCash = self.todayCash.ws.cell(column=populationColumn, row=populationRow).value
+
+        religionRow = self.todayCash.findCellByStr("Релігійні організації", "C").row
+        religionColumn = populationColumn # the same column
+        religionCash = self.todayCash.ws.cell(column=religionColumn, row=religionRow).value
+
+        return populationCash + religionCash
+
+    def Teploenergy(self):
+        """
+        """
+        teploenergyRow = self.todayCash.findCellByStr("3.2. Теплоенергетика за прямими договорами", "C").row
+        teploenergyColumn = openpyxl.utils.column_index_from_string(str("J"))
+        teploenergyCash = self.todayCash.ws.cell(column=teploenergyColumn, row=teploenergyRow).value
+
+        kyivEnergoNotEeRow = 
+        
 
 
 
--- main.py
+++ main.py
@@ -3,5 +3,10 @@ from FiscalPlan.FiscalPlan import FiscalPlan
 
 if __name__ == "__main__":
     print("I`m main file")
-    excelBook = FiscalPlan(r"FiscalPlan\НадходженняНаКР_.xls")
+    excelBook = FiscalPlan(r"FiscalPlan\09_Прогнозне надходження коштів у вересні 2019 року.xlsx",
+                            r"FiscalPlan\НадходженняНаКР_июнь.xls",
+                            r"FiscalPlan\Всі Категорії. (без спожив. за СЕРПЕНЬ)_ПАТ.xls",
+                            r"FiscalPlan\Всі Категорії. (без спожив. за СЕРПЕНЬ)_ПАТ.xls",
+                            r"FiscalPlan\ТЕЦ.xls"
+                            )
     excelBook.run()
