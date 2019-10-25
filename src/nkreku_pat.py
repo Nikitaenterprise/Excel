47b6f5b0be68258ee3717ac6411da90766eea882
Лужанский Никита Вячеславович <LuzhanskyiM@naftogaz.com>
2019-10-25 15:40:02 +0300

    Working on findInSaldo on operators AND OR

--- src/nkreku_pat.py
+++ src/nkreku_pat.py
@@ -54,31 +54,34 @@ class NKREKU_PAT(Algorithm):
         self.saldo.open(data_only=True)
         saldoWs = self.saldo.getWs()
 
-        listForTest1 = []
-        time1 = time.time()
+        columnToWriteList = ["C", "D", "E"]
+        columnList = []
+        for column in columnToWriteList:
+            columnList.append(openpyxl.utils.column_index_from_string(column))
+
+        
+        
         rangeIter = "B9" + ":" + "B" + str(VTVWs.max_row)
         for cells in VTVWs[rangeIter]:
             for cell in cells:
                 if cell.value != None:
                     company = cell.value
 
-                    columnC = self.findInSaldo(saldoWs, company, None, None, "G")
-                    columnD = self.findInSaldo(saldoWs, company, None, None, "H")
-                    columnE = self.findInSaldo(saldoWs, company, None, None, "I")
+                    data = self.findInSaldo2(saldoWs, company, None, None, whatColumns=["G", "H", "I"])
+                    for i in range(0, len(data)):
+                        VTVWs.cell(column=columnList[i], row=cell.row).value = data[i]
+
+                    data = self.findInSaldo2(saldoWs, company, None, ["2019"], whatColumns=["K", "L", "S"])
+                    for i in range(0, len(data)):
+                        # i + 4 to shift columns from C D E to G H I
+                        VTVWs.cell(column=columnList[i]+4, row=cell.row).value = data[i]
+
+                    data = self.findInSaldo2(saldoWs, company, None, ["!2019"], whatColumns=["K", "L", "S"])
+                    for i in range(0, len(data)):
+                        # i + 4 to shift columns from C D E to K L M
+                        VTVWs.cell(column=columnList[i]+8, row=cell.row).value = data[i]
                     
-        print(time.time()-time1)
-
-        time1 = time.time()
-        rangeIter = "B9" + ":" + "B" + str(VTVWs.max_row)
-        for cells in VTVWs[rangeIter]:
-            for cell in cells:
-                if cell.value != None:
-                    company = cell.value
-
-                    column = self.findInSaldo2(saldoWs, company, None, None, "G")
-                    
-        print(time.time()-time1)
-
+        self.VTV.save(self.VTV.pathToFile, "111", extension=".xlsx")
 
 
     def findInSaldo(self, saldoSheet, whatToFind: str, 
@@ -141,86 +144,112 @@ class NKREKU_PAT(Algorithm):
         return
 
     def findInSaldo2(self, saldoSheet, whatToFind: str, 
-                    whatCategory: list, whatResource: list, whatColumn: str, inWhatColumnFind="A"):
+                    whatCategory: list, whatResource: list, whatColumns: list, inWhatColumnFind="A"):
         """
         """
         numberOfRows = saldoSheet.max_row
         rangeIter = inWhatColumnFind + "10" + ":" + inWhatColumnFind + str(numberOfRows)
         columnCategory = openpyxl.utils.column_index_from_string("C")
         columnResource = openpyxl.utils.column_index_from_string("F")
-        columnWithData = openpyxl.utils.column_index_from_string(whatColumn)
-        columnWithData1 = openpyxl.utils.column_index_from_string("G")
-        columnWithData2 = openpyxl.utils.column_index_from_string("H")
-        columnWithData3 = openpyxl.utils.column_index_from_string("I")
 
-        columnsList = [columnWithData1, columnWithData2, columnWithData3]
+        listOfColumns = []
+        for column in whatColumns:
+            col = openpyxl.utils.column_index_from_string(column)
+            listOfColumns.append(col)
+
+        exclusionListCategory = []
+        if whatCategory != None:
+            for cat in whatCategory:
+                if "!" in cat:
+                    catList = cat.split("!")
+                    exclusionListCategory.append(catList[1])
+        if not exclusionListCategory:
+            exclusionListCategory.append("")
+
+        exclusionListResource = []
+        if whatResource != None:
+            for res in whatResource:
+                if "!" in res:
+                    resList = res.split("!")
+                    exclusionListResource.append(resList[1]) 
+        if not exclusionListResource:
+            exclusionListResource.append("")
+
         for cells in saldoSheet[rangeIter]:
             for cell in cells:
                 # If company name equals to what to find variable
                 if cell.value != None and cell.value == whatToFind:
-                    # Iterate throug this company data
-                    returnValue1 = 0
-                    returnValue2 = 0
-                    returnValue3 = 0
+                    # Iterate through this company data
                     row = cell.row
+
+                    returnValuesList = [0]*len(whatColumns)
                     while True:
                         # Move 1 row down
                         row += 1
                         category = saldoSheet.cell(column=columnCategory,
-                                                row=row).value
+                                                    row=row).value
                         # If its None then we know that company data ends
                         # and other company begins
                         if category == None:
                             break
                         # Transform int value of resource into str 2019 -> "2019"
                         resource = str(saldoSheet.cell(column=columnResource,
-                                                row=row).value).strip()
-                        value1 = saldoSheet.cell(column=columnsList[0],
-                                                row=row).value
-                        value2 = saldoSheet.cell(column=columnsList[1],
-                                                row=row).value
-                        value3 = saldoSheet.cell(column=columnsList[2],
-                                                row=row).value
-                        if value1 == None:
-                            value1 = 0
-                        if value2 == None:
-                            value2 = 0
-                        if value3 == None:
-                            value3 = 0
+                                                        row=row).value).strip()
+                        
+                        # Get values from different columns
+                        # Write them to list
+                        valuesList = []
+                        for column in listOfColumns:
+                            value = saldoSheet.cell(column=column,
+                                                    row=row).value
+                            if value == None:
+                                value = 0
+                            valuesList.append(value)
+                        
+                        willBeCalculated = False
+
+                        # If just category or resource was specified without excluding
+                        # values. for example ["2019"] or ["ТЕ"]
+                        #if "!" not in whatCategory and "!" not in whatResource:
                         # If both category and resource 
                         # are not specified then add all values
                         if not whatCategory and not whatResource:       
-                            returnValue1 += value1
-                            returnValue2 += value2
-                            returnValue3 += value3
+                            willBeCalculated = True
                         # If category is specified and resource aren`t
                         elif whatCategory and not whatResource:
-                            if category in whatCategory:
-                                returnValue1 += value1
-                                returnValue2 += value2
-                                returnValue3 += value3
+                            if category in whatCategory or category not in exclusionListCategory:
+                                willBeCalculated = True
+                            elif category in exclusionListCategory:
+                                willBeCalculated = False
+
                         # If resource is specified and category aren`t
                         elif whatResource and not whatCategory:
-                            if resource in whatResource:
-                                returnValue1 += value1
-                                returnValue2 += value2
-                                returnValue3 += value3
+                            if resource in whatResource or resource not in exclusionListResource:
+                                willBeCalculated = True
+                            if resource in exclusionListResource:
+                                willBeCalculated = False
                         # If both are specified
                         elif whatCategory and whatResource:
-                            if category in whatCategory and resource in whatResource:
-                                returnValue1 += value1
-                                returnValue2 += value2
-                                returnValue3 += value3
+                            if category in whatCategory and\
+                                    resource in whatResource or\
+                                    category not in exclusionListCategory and\
+                                    resource not in exclusionListResource:
+                                willBeCalculated = True
+                            if category in exclusionListCategory or\
+                                    resource in exclusionListResource:
+                                willBeCalculated = False
+                            
+
+                        if willBeCalculated == True:
+                            for i in range(0, len(valuesList)):
+                                returnValuesList[i] += valuesList[i]
+                        # If excluding values are in resource or category
+                        # For example ["!2019"] or ["!ТЕ"]
+                        #elif "!" in whatCategory or "!" in whatResource:
+
                     
-                    return returnValue1, returnValue2, returnValue3
-        
-        # If company wasn`t found in saldo then set return value to 0
-        try:
-            returnValue1, returnValue2, returnValue3
-        except UnboundLocalError:
-            returnValue1=0
-            returnValue2=0 
-            returnValue3 = 0
-            return returnValue1, returnValue2, returnValue3
-        
-        return
+                    return returnValuesList
+
+        # If no data waas found then return zeros
+        returnValuesList = [0]*len(listOfColumns)
+        return returnValuesList
